82670d637e3c66d0f7d8464a43a7f87b66800594
Le Dinh Kien <s3515300@rmit.edu.vn>
2016-06-23 01:18:33 +0700

    Add/Remove fields

--- stylesheets/gist.js
+++ stylesheets/gist.js
@@ -4,13 +4,13 @@ $('ul.nav li.dropdown').hover(function () {
     $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
 });
 
-function validateForm() {
-    var x = document.forms["myForm"]["name"].value;
-    if (x == null || x == "") {
-        alert("Product name must be filled out");
-        return false;
-    }
-}
+// function validateForm() {
+//     var x = document.forms["myForm"]["name"].value;
+//     if (x == null || x == "") {
+//         alert("Product name must be filled out");
+//         return false;
+//     }
+// }
 
 //        function addForm() //}
 //            $form = $("<form></form>");
